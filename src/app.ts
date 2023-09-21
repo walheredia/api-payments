8311fdb3c2982e6d020d4823766dd55397d110af
Walter Heredia <wal_dh@hotmail.com>
2023-09-21 16:36:39 -0300

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import * as dotenv from 'dotenv';
 dotenv.config()
 const app = express();
+app.use(cors())
 import applyMiddlewares from './middlewares';
 
 // Middlewares
